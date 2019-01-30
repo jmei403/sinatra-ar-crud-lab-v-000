a32bdc0c67a33bc49ff633e89254a723063a2cbc
Jason Mei <jason.mei403@gmail.com>
2019-01-30 23:51:32 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,18 +1,5 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
-  #
-  # attr_reader :title, :content
 
-  # @@all = []
-  #
-  # def initialize(params)
-  #   @title = params[:title]
-  #   @content = params[:content]
-  #   @@all << self
-  # end
-  #
-  # def self.all
-  #   @@all
-  # end
 end
