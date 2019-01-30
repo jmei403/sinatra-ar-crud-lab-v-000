7fd9b9ee210b8410171d52e03d016357e20198f5
Jason Mei <jason.mei403@gmail.com>
2019-01-30 23:39:11 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,17 +2,17 @@
 
 class Article < ActiveRecord::Base
   #
-  attr_reader :title, :content
+  # attr_reader :title, :content
 
-  @@all = []
-
-  def initialize(params)
-    @title = params[:title]
-    @content = params[:content]
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
+  # @@all = []
+  #
+  # def initialize(params)
+  #   @title = params[:title]
+  #   @content = params[:content]
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   @@all
+  # end
 end
