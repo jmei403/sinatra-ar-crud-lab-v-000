5a30dd7063117b6497e17a5893015f77dc107346
Jason Mei <jason.mei403@gmail.com>
2019-01-30 22:33:36 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,27 +4,19 @@ class Article < ActiveRecord::Base
 
   attr_reader :title, :content
 
-<<<<<<< HEAD
   ARTICLES = []
-=======
-  @@all = []
->>>>>>> 79639980e6c76972cad0fe12449657f3f698ca6c
 
   def initialize(params)
     @title = params[:title]
     @content = params[:content]
-<<<<<<< HEAD
     ARTICLES << self
   end
 
   def self.all
-    ARTICLES
-=======
     @@all << self
   end
 
   def self.all
     @@all
->>>>>>> 79639980e6c76972cad0fe12449657f3f698ca6c
   end
 end
