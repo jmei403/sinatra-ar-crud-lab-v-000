5557923283d7b2a544ec0d320e9eb06c344e0b25
Jason Mei <jason.mei403@gmail.com>
2019-01-31 00:34:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/articles/:id' do
-    @article = Article.find(params[:id])
+    @article = Article.all.find(params[:id])
 
   end
 end
