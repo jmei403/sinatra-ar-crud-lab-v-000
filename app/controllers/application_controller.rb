5af75f10a4a08ca6f83d7e698e5b32e3c83e5002
Jason Mei <jason.mei403@gmail.com>
2019-01-31 01:03:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
 
   post '/articles' do
     @article = Article.create(title: params[:title], content: params[:content])
-    erb :show
+    redirect to "/articles/#{@article.id}"
   end
 
   get '/articles' do
